a1b5ee4ad9660809bef598aa0fae2ae713b13a78
alienfacepalm <brandon@alienfacepalm.com>
2017-03-12 23:01:43 -0700

    first

---
+++ src/main/java/com/afp/AfpApplication.java
@@ -0,0 +1,12 @@
+package com.afp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfpApplication.class, args);
+	}
+}
